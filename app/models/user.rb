41d8a3ebea23e3e3f6c0eed9e03b43f84ac83ee6
HansHauge <hansthered@gmail.com>
2017-03-09 11:03:03 -0800

    HKH only game masters can see the edit, delete and new buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  ROLES = ['game_player', 'game_master', '']
+  validates :role, inclusion: { in: ROLES }
+
+  def game_master?
+    self.role.eql?('game_master')
+  end
 end
